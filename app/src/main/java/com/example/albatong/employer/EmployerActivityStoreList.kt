c01ccbbb7151c9e53ea1d654efebcebdd9893e5f
DingWoonee <136048882+DingWoonee@users.noreply.github.com>
2023-06-09 15:36:24 +0900

    사장님 메인 뒤로가기 설정
    2초내로 2번 누르면 꺼집니다.

--- app/src/main/java/com/example/albatong/employer/EmployerActivityStoreList.kt
+++ app/src/main/java/com/example/albatong/employer/EmployerActivityStoreList.kt
@@ -4,6 +4,7 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.appcompat.app.AlertDialog
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.albatong.data.*
@@ -26,6 +27,7 @@ class EmployerActivityStoreList : AppCompatActivity() {
     lateinit var sdb: DatabaseReference
     lateinit var layoutManager: LinearLayoutManager
     var user_id:String? = null
+    private var backKeyPressedTime: Long = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -49,6 +51,19 @@ class EmployerActivityStoreList : AppCompatActivity() {
     override fun onStop() {
         super.onStop()
         adapter.stopListening()
+    }
+
+    override fun onBackPressed() {
+        // 현재 시간이 마지막으로 뒤로 가기 버튼을 눌렀던 시간보다 2초 이상 크면
+        if (System.currentTimeMillis() > backKeyPressedTime + 2000) {
+            backKeyPressedTime = System.currentTimeMillis()
+            Toast.makeText(this, "한 번 더 누르면 종료됩니다.", Toast.LENGTH_SHORT).show()
+            return
+        }
+        // 마지막 '뒤로 가기'버튼 누르기 후, 2초가 지나지 않은 상태에서 '뒤로 가기'버튼을 누르면
+        if (System.currentTimeMillis() <= backKeyPressedTime + 2000) {
+            finish() // 앱 종료
+        }
     }
 
     fun init() {
