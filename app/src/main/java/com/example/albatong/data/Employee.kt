c1cc6b326b6b0bbc818e30767c21d8efe78ef947
light11014 <83333513+light11014@users.noreply.github.com>
2023-06-09 13:51:16 +0900

    test 파일 삭제

--- app/src/main/java/com/example/albatong/data/Employee.kt
+++ app/src/main/java/com/example/albatong/data/Employee.kt
@@ -4,7 +4,4 @@ data class Employee(var employeeId: String,
                     var name: String,
                     var salary: Int=10000) {
     constructor(): this("no-data", "no-data")
-
-    // 20230609 test
-    // 20230609 test5
 }
