e2ebbff4072f3e65481231cc4b463c002d5a4a55
light11014 <83333513+light11014@users.noreply.github.com>
2023-06-09 13:15:16 +0900

    230609 test

--- app/src/main/java/com/example/albatong/data/Employee.kt
+++ app/src/main/java/com/example/albatong/data/Employee.kt
@@ -4,4 +4,6 @@ data class Employee(var employeeId: String,
                     var name: String,
                     var salary: Int=10000) {
     constructor(): this("no-data", "no-data")
+
+    // 20230609 test
 }
