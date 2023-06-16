6599d45edc49de319cadf3ff912c1b8f674530c6
김현지 <freeziahj@gmail.com>
2023-06-16 11:00:37 +0900

    알림 및 설정 ui

--- app/src/main/java/com/example/albatong/er/ERActivitySpecificMain.kt
+++ app/src/main/java/com/example/albatong/er/ERActivitySpecificMain.kt
@@ -10,6 +10,7 @@ import android.widget.ImageButton
 import androidx.core.widget.ImageViewCompat
 import com.example.albatong.R
 import com.example.albatong.databinding.ErActivitySpecificMainBinding
+import com.example.albatong.login.LoginActivity
 import com.example.albatong.login.SignAcitivity
 import com.google.android.material.tabs.TabLayoutMediator
 
@@ -17,11 +18,12 @@ class ERActivitySpecificMain : AppCompatActivity() {
     val textarr = arrayListOf<String>("일정 관리", "직원 관리", "인수인계" , "공지사항")
     val imgarr = arrayListOf<Int>(R.drawable.baseline_calendar_month_24, R.drawable.baseline_people_alt_24,R.drawable.baseline_content_paste_go_24,R.drawable.baseline_access_alarm_24)
     lateinit var binding: ErActivitySpecificMainBinding
+    val userid = LoginActivity.uId
+
     override fun onCreate(savedInstanceState: Bundle?) {
         binding = ErActivitySpecificMainBinding.inflate(layoutInflater)
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
-
 
         setSupportActionBar(binding.toolbar)
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
@@ -41,8 +43,10 @@ class ERActivitySpecificMain : AppCompatActivity() {
             val i = Intent(this@ERActivitySpecificMain, ERsettingActivity::class.java)
             startActivity(i)
         }
+        val userID = intent.getStringExtra("user_id")
         binding.employerNotificationHistoryButton.setOnClickListener {
             val i = Intent(this@ERActivitySpecificMain, SignAcitivity::class.java)
+            intent.putExtra("user_id", userID)
             startActivity(i)
         }
     }
